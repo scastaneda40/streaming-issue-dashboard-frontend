e1c6c06f8535a7d955698fb6686cf59da197b2e5
Stephen Castaneda <stephencastaneda@Mac.lan>
2025-10-29 22:06:01 -0700

    Add Webpack alias for Apollo Client hooks

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
+const path = require("path");
+
 const nextConfig = {
   transpilePackages: [
     "@chakra-ui/react",
     "@chakra-ui/alert",
     "@chakra-ui/form-control",
   ],
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@apollo/client/react/hooks": path.resolve(
+        __dirname,
+        "node_modules/@apollo/client/react/hooks/useQuery.js" // This path might need adjustment
+      ),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
